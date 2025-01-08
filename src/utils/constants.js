bafd8d3a3b3cf9f2d924c04dca7c42ac6de933ec
Arxyzc <arxyzc@outlook.com>
2025-01-08 00:04:53 -0600

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 export const HOST = import.meta.env.VITE_SERVER_URL;
 
+console.log("VITE_SERVER_URL:", HOST);
+
 export const AUTH_ROUTES = "api/auth";
 export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
 export const LOGIN_ROUTE = `${AUTH_ROUTES}/login`;
